c4b3c79c1774a866a5a1d5a02626ecd0b4cb5b85
Kyle Huggins <5430818+kjhx@users.noreply.github.com>
2019-09-18 17:27:21 -0500

    Enable webfonts. Update dependencies & styles.

--- bin/render.js
+++ bin/render.js
@@ -2,23 +2,29 @@ const puppeteer = require('puppeteer');
 
 var file = process.argv[2];
 var file_fullpath = __dirname;
-file_fullpath = file_fullpath.substring(0, file_fullpath.length - 4);
-file_fullpath += '/out/html/' + file;
+file_fullpath = file_fullpath.substring(0, file_fullpath.length - 4) + '/out/html/' + file;
 
 var out_file = process.argv[3];
 
-// console.log('Rendering ' + file_fullpath + ' to ' + out_file);
+console.log(file_fullpath + ' -> ' + out_file);
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
+    await page.setCacheEnabled(false);
 
     await page.setViewport({
         width: 1280,
         height: 600
     })
 
-    await page.goto('file://' + file_fullpath);
+    try {
+        await page.goto('file://' + file_fullpath, { waitUntil: 'networkidle0' });
+    } catch (exception) {
+        console.log(exception + '\nPlease report this error at https://github.com/kjhx/resume.md/issues.');
+    }
+
     await page.emulateMedia('screen');
     await page.pdf({
         path: out_file,
